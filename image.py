4edd0fa3229bedbe418d039e4ee160f4b57f3c6c
Matthew Jordan <matthewsages@gmail.com>
2022-06-22 13:11:03 -0700

    Update

--- image.py
+++ image.py
@@ -1,6 +1,7 @@
 import io
 import numpy as np
 import cv2
+
 from PIL import Image
 
 
@@ -26,16 +27,59 @@ def downsample(img, scale=4):
     cb = np.kron(cb, np.ones((scale, scale)))[:img.shape[0],:img.shape[1]]
     cr = np.kron(cr, np.ones((scale, scale)))[:img.shape[0],:img.shape[1]]
 
-    img = np.stack([y, cb, cr], axis=2).astype(np.uint8)
+    img = np.stack([y, cb, cr], axis=2)
 
     return img
 
 
-img = Image.open("images/cat.png")
+def split_blocks(img, size=8):
+    x_overflow = img.shape[0] % size
+    y_overflow = img.shape[1] % size
+    img = img[:-x_overflow,:-y_overflow,:]
+
+    y = img[:,:,0]
+    print(y[0:8,0:8,])
+
+    y = np.ravel(y)
+    y = np.split(y, y.shape[0] // size)
+    print(y)
+
+    # print()
+    # print()
+    # print()
+    # print()
+    # print()
+    # print()
+    # print()
+    # print()
+    # y = y.reshape(8, 8, -1, order="F")
+    # print(y.shape)
+    # print(y[0:8,0:8,0])
+    # cb = img[:,:,1]
+    # cr = img[:,:,2]
+
+    # y = [x.reshape(8, 8) for x in np.split(y, y.shape[0] // size**2)]
+    # print(y[0])
+    # print(y.shape[0])
+
+    # y = np.split(y.ravel(), img.shape[])
+    # print(len(y))
+    # for i in y:
+        # print(i.shape)
+
+
+# def DCT(block):
+    # block -= 128
+
+
+img = Image.open("images/wood.png")
 img = rgb_to_ycbcr(img)
 img = downsample(img)
+split_blocks(img)
+# y, cb, cr = split_blocks(img)
+# img = DCT(blocks[0])
 
 
-img = cv2.cvtColor(img, cv2.COLOR_YCrCb2RGB)
-PIL_image = Image.fromarray(img).convert("YCbCr")
-PIL_image.show()
+# img = cv2.cvtColor(np.uint8(img), cv2.COLOR_YCrCb2RGB)
+# PIL_image = Image.fromarray(img).convert("YCbCr")
+# PIL_image.show()
