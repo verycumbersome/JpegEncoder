3de943c12e856eeb6713f95c927426f6bde7cfac
Matthew Jordan <matthewsages@gmail.com>
2022-06-24 20:12:08 -0700

    Update compression rate for JPEG

--- main.py
+++ main.py
@@ -1,52 +1,100 @@
-import random
+import io
+import cv2
+import time
 import numpy as np
-import matplotlib.pyplot as plt
 
-N = 100  # Length of signal
-K = 32  # Length of vosine matrix
+from PIL import Image
 
+class JPEG():
+    def __init__(self, fp, compression=8, N=8):
+        self.img = np.array(Image.open(fp).convert())
 
-def cosine(n, k):
-    """Function: Cos((pi*k/2N) * (2N+1)); Sum for n=0 to N"""
-    return np.cos((2 * k + 1) * (n * np.pi / (2 * N)))
+        # Constants
+        self.N = N
+        self.T = np.array([self.cosine(x, np.arange(N)) for x in range(N)])
+        self.Q = np.array([
+            [16, 11, 10, 16, 24, 40, 51, 61],
+            [12, 12, 14, 19, 26, 58, 60, 55],
+            [14, 13, 16, 24, 40, 57, 69, 56],
+            [14, 17, 22, 29, 51, 87, 80, 62],
+            [18, 22, 37, 56, 68,109,103, 77],
+            [24, 35, 55, 64, 81,104,113, 92],
+            [49, 64, 78, 87,103,121,120,101],
+            [72, 92, 95, 98,112,100,103, 99]
+        ])[:N,:N]
+        self.compression = compression
+        self.rgb_to_ycbcr()
+        self.downsample()
 
+    def rgb_to_ycbcr(self):
+        R = self.img[:,:,0]/256.
+        G = self.img[:,:,1]/256.
+        B = self.img[:,:,2]/256.
+        self.img[:,:,0] = 16. + 65.738 * R + 129.057 * G + 25.064 * B
+        self.img[:,:,1] = 128. - 37.945 * R - 74.494 * G + 112.439 * B
+        self.img[:,:,2] = 128. + 112.439 * R - 94.154 * G - 18.285 * B
 
-def DCT(x):
-    """Find all composite cosine frequencies via matrix multiplication"""
-    C = np.fromfunction(lambda i, j: cosine(i, j), (K, N), dtype=float)
-    out = np.matmul(C, x)
-    print(out.astype(int))
-    return out
+    def downsample(self, scale=4):
+        img = self.img
+        cb = img[::scale,::scale,1]
+        cr = img[::scale,::scale,2]
+        y = img[:,:,0]
+        cb = np.kron(cb, np.ones((scale, scale)))[:img.shape[0],:img.shape[1]]
+        cr = np.kron(cr, np.ones((scale, scale)))[:img.shape[0],:img.shape[1]]
+        self.img = np.stack([y, cb, cr], axis=2)
 
+    def encode(self):
+        img = np.copy(self.img)
+        for ch in range(3):
+            for x in range(self.img.shape[0] // self.N - 1):
+                for y in range(self.img.shape[1] // self.N - 1):
+                    img[x*self.N:(x+1)*self.N, y*self.N:(y+1)*self.N,ch] = self.DCT(img, x, y, ch)
+                    img[x*self.N:(x+1)*self.N, y*self.N:(y+1)*self.N,ch] = self.IDCT(img, x, y, ch)
+        self.show(img)
 
-def main():
-    # Pure cos signal for testing
-    period = 4
-    x_1 = np.cos(np.linspace(-np.pi, np.pi, N) * period / 2)
-    x_1 += np.cos(np.linspace(-np.pi, np.pi, N) * 8 / 2)
-    out1 = DCT(x_1)
+    def cosine(self, i, j):
+        """Row-wise function for populating DCT matrix"""
+        if i == 0:
+            out = np.ones(self.N) / np.sqrt(self.N)
+        else:
+            out = np.sqrt(2/self.N) * np.cos(((2*j+1) * i * np.pi) / (2 * self.N))
 
-    # Random signal generated for decomposition
-    periods = random.sample(range(0, 16), 2)
-    x_2 = np.cos(np.linspace(-np.pi, np.pi, N) * periods[0] / 2)
-    x_2 += np.cos(np.linspace(-np.pi, np.pi, N) * periods[1] / 2)
-    out2 = DCT(x_2)
+        return out
 
-    # Plotting
-    fig, axs = plt.subplots(2, 2)
-    fig.suptitle("Discrete Cosine Transform")
-    fig1_domain = np.linspace(0, np.pi * 5, N)
-    fig2_domain = np.arange(K)
-    axs[0, 0].set_title("Sum of cos(4x) and cos(8x)")
-    axs[0, 1].set_title("Sum of cos({}x) and cos({}x)".format(periods[0], periods[1]))
-    axs[1, 0].set_title("")
-    axs[1, 1].set_title("")
-    axs[0, 0].plot(fig1_domain, x_1)
-    axs[0, 1].plot(fig1_domain, x_2)
-    axs[1, 0].plot(fig2_domain, out1)
-    axs[1, 1].plot(fig2_domain, out2)
-    plt.show()
+    def DCT(self, img, x, y, ch):
+        """Performs the DCT operation on an 8x8 block
+        x, y: the indices for each 8x8 block
+        ch: channel from index 0 to 2 for y, cb, cr
+        """
+        block = img[x*self.N:(x+1)*self.N, y*self.N:(y+1)*self.N,ch]
+        D = np.matmul(np.matmul(self.T, block - 128.), self.T.T)
+        C = np.rint(D / (self.Q * self.compression))
+
+        return C
+
+    def IDCT(self, img, x, y, ch):
+        """Performs the inverse DCT operation on an 8x8 block
+        x, y: the indices for each 8x8 block
+        ch: channel from index 0 to 2 for y, cb, cr
+        """
+        block = img[x*self.N:(x+1)*self.N, y*self.N:(y+1)*self.N,ch]
+        R = (self.Q * self.compression) * block
+        out = np.rint(np.matmul(np.matmul(self.T.T, R), self.T) + 128.)
+
+        return out
+
+    def show(self, img):
+        img = cv2.cvtColor(np.uint8(img), cv2.COLOR_YCrCb2RGB)
+        cv2.imshow('image', img)
+        cv2.waitKey(1)
 
 
 if __name__=="__main__":
-    main()
+    jpeg = JPEG("images/lion.png", compression=0, N=8)
+
+    while True:
+        jpeg.compression += 0.1
+        jpeg.encode()
+
+        print(jpeg.compression)
+
